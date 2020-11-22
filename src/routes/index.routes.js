ca1bab6878efe7af8116fac5c1933ec232350996
JulioRodriguezS <julio.rodriguez.soberano@gmail.com>
2020-11-22 12:36:53 -0500

    uploading just for anything

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -7,13 +7,20 @@ const uuid = require('uuid');
 const multerStorageConf = multer.diskStorage({
     destination: path.join(__dirname, '../public/images'),
     filename: (req, file, cb) => {
-        cb(null, uuid.v4() + '.' + file.mimetype.split('/')[1])
+        cb(null, uuid.v4() + path.extname(file.originalname))
     }
 });
+const multerFileFilter = (req, file, cb) => {
+    const filetypes = /jpeg|jpg|png|gif/;
+    const extname = filetypes.test(path.extname(file.originalname));
+    if (extname) return cb(null, true);
+    cb('err: file not supported');
+}
 const upImg = multer({
     storage: multerStorageConf,
     dest: path.join(__dirname, '../public/images'),
-    limits: {fieldSize: 1000000}
+    limits: { fieldSize: 1000000 },
+    fileFilter: multerFileFilter
 }).single('imgFile');
 
 // routes
@@ -23,6 +30,6 @@ router
     })
     .post('/upload', upImg, async (req, res, next) => {
         console.log(req.file);
-        setTimeout(()=>{res.redirect('/')},2000);
+        setTimeout(() => { res.redirect('/') }, 2000);
     })
 module.exports = router;
